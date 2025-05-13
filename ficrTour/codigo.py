f11e5fccc5f9a397f81c6b622078960dd8205df6
eduardo de melo <duduperuano@outlook.com>
2025-05-13 14:33:47 -0300

    criaçao do login

--- ficrTour/codigo.py
+++ ficrTour/codigo.py
@@ -5,18 +5,34 @@ from selenium.webdriver.common.by import By
 import time
 import os
 
-# Caminho absoluto para o arquivo HTML
-#aqui voce vai por de acordo com seu pc
-caminho_html = 'C:\Users/eduardo/Downloads/ficrTour/index.html'
-
-# Iniciar o Chrome
+caminho_html = "C:/Users/eduardo/Downloads/ficrTour/login.html"
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-# Abrir o HTML local
 driver.get(caminho_html)
+time.sleep(4)
+usuario = driver.find_element(By.ID, "usuario")
+senha = driver.find_element(By.ID, "senha")
+usuario.send_keys("admin")
+senha.send_keys("123")
+time.sleep(4)
+usuario.clear()
+senha.clear()
+time.sleep(3)
+button = driver.find_element(By.ID, "btnLogin")
+button.click()
+time.sleep(4)
+print(f" a url atual é: {driver.current_url}")
+url_home = "file:///C:/Users/eduardo/Downloads/ficrTour/index.html"
+if driver.current_url == url_home:
+    print("redirecionamento bem-sucedido!")
+else:
+    print(f"redirecionamento falhou. URL atual: {driver.current_url}")
 
-# Esperar um pouco para visualizar
+checkbox = driver.find_element(By.ID, "aceite")
+if not checkbox.is_selected():
+    print("a caixa de selecao nao esta marcada inicialmente")
+checkbox.click()
 time.sleep(2)
-
-# Fechar o navegador
+if checkbox.is_selected():
+    print("a caixa de selecao esta marcada")
+time.sleep(30)
 driver.quit()
