1ffb1631b6c95e4211260eb7e77e9829c3013784
lauraaoliveira <100322556+lauraaoliveira@users.noreply.github.com>
2025-05-15 12:27:24 -0300

    fix: marcação de vários checkboxes

--- ficrTour/codigo.py
+++ ficrTour/codigo.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 import time
 import os
 
-caminho_html = "C:/Users/eduardo/Downloads/ficrTour/login.html"
+caminho_html = "C:/Users/Administrador/Desktop/ficrTour/login.html"
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get(caminho_html)
 time.sleep(4)
@@ -21,13 +21,13 @@ button = driver.find_element(By.ID, "btnLogin")
 button.click()
 time.sleep(4)
 print(f" a url atual é: {driver.current_url}")
-url_home = "file:///C:/Users/eduardo/Downloads/ficrTour/index.html"
+url_home = "file:///C:/Users/Administrador/Desktop/ficrTour/index.html"
 if driver.current_url == url_home:
     print("redirecionamento bem-sucedido!")
 else:
     print(f"redirecionamento falhou. URL atual: {driver.current_url}")
 time.sleep(5)
-url_roteiro = "file:///C:/Users/eduardo/Downloads/ficrTour/roteiro.html"
+url_roteiro = "file:///C:/Users/Administrador/Desktop/ficrTour/roteiro.html"
 driver.get(url_roteiro)
 time.sleep(4)
 if driver.current_url == url_roteiro:
@@ -35,12 +35,21 @@ if driver.current_url == url_roteiro:
 else:
     print(f"redirecionamento para roteiro falhou. URL atual: {driver.current_url}")
 time.sleep(5)
-checkbox = driver.find_element(By.ID, "aceite")
-if not checkbox.is_selected():
-    print("a caixa de selecao nao esta marcada inicialmente")
-checkbox.click()
-time.sleep(2)
-if checkbox.is_selected():
-    print("a caixa de selecao esta marcada")
-time.sleep(30)
+ids_checkboxes = ["casal", "verao", "aventura", "hotel-incluso"]
+
+for checkbox_id in ids_checkboxes:
+    checkbox = driver.find_element(By.ID, checkbox_id)
+    if not checkbox.is_selected():
+        print(f"A caixa de seleção '{checkbox_id}' não está marcada inicialmente")
+        checkbox.click()
+        time.sleep(1)
+    else:
+        print(f"A caixa de seleção '{checkbox_id}' já estava marcada")
+    if checkbox.is_selected():
+        print(f"A caixa de seleção '{checkbox_id}' está marcada agora")
+    else:
+        print(f"A caixa de seleção '{checkbox_id}' **não** foi marcada corretamente")
+    time.sleep(1)
+
+time.sleep(5)
 driver.quit()
